1c437463bcfe7b71d782737cc3226c872bbe39df
Fattan-malva <fattanmalva14@gmail.com>
2025-10-20 17:38:45 +0700

    [UP]JWT Token

--- src/Routes/R.TrxSchedule.js
+++ src/Routes/R.TrxSchedule.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const ScheduleController = require('../Controllers/C.TrxSchedule');
+const { authenticateToken } = require('../Auth/middleware');
 
 // GET schedule berdasarkan parameter (date, roomType, studioID)
 router.get('/', ScheduleController.getSchedule);
 // GET schedule berdasarkan date saja
-router.get('/by-date', ScheduleController.getScheduleByDate);
+router.get('/by-date', authenticateToken, ScheduleController.getScheduleByDate);
 // GET schedule berdasarkan date dan studio saja
-router.get('/by-date-studio', ScheduleController.getScheduleByDateAndStudio);
+router.get('/by-date-studio', authenticateToken, ScheduleController.getScheduleByDateAndStudio);
 // GET schedule berdasarkan date dan room type saja
-router.get('/by-date-roomtype', ScheduleController.getScheduleByDateAndRoomType);
+router.get('/by-date-roomtype', authenticateToken, ScheduleController.getScheduleByDateAndRoomType);
 
 module.exports = router;
