4ea9f7a42b9ad2b6186984be2d521701bca6fbdf
Fattan-malva <fattanmalva14@gmail.com>
2025-10-23 13:32:04 +0700

    [UPP]Query Studio

--- src/Models/M.MstStudio.js
+++ src/Models/M.MstStudio.js
@@ -2,7 +2,7 @@ const { getPool, sql } = require('../Config/db');
 
 async function findAll() {
     const pool = await getPool();
-    const result = await pool.request().query('SELECT * FROM MstStudio');
+    const result = await pool.request().query("SELECT * FROM MstStudio WHERE trainingSite = '1'");
     return result.recordset;
 }
 module.exports = { findAll };
